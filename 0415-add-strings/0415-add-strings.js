0aa5f37ce22dcc133f6196dc1b93aa2e433cce3f
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2023-10-25 22:16:34 +0530

    Time: 51 ms (91.12%), Space: 42.3 MB (83.8%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+     const c=  BigInt(num1)+BigInt(num2);
+     return c+"";
+    
+};
