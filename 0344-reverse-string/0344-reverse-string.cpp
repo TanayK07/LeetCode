a14e837099a249597a33889573596d3a56f2342c
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-06-02 15:20:22 +0530

    Time: 16 ms (68.47%), Space: 26.9 MB (89.96%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    void reverseHelper(vector<char>& s, int i, int j) {
+        if (i >= j)
+            return;
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        reverseHelper(s, i+1, j-1);
+    }
+    
+public:
+    void reverseString(vector<char>& s) {
+        reverseHelper(s, 0, s.size() - 1);
+    }
+};
