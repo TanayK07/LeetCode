41282d6cb00f504e24f1dfd56c363205fe81fdaf
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-06-04 12:28:51 +0530

    Time: 2 ms (63.38%), Space: 8.8 MB (62.59%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int> m1;
+        int f=0,c=0,co=0;
+        for (char ch : s) m1[ch]++;
+    for (const auto& it : m1) {
+            cout << it.first << " " << it.second << endl;
+        }
+        for (const auto& it : m1) {
+                             
+                c+=it.second;
+                if(it.second%2!=0) 
+                {
+                    co++;
+                    f=1;
+
+                }
+                
+
+          }
+          return c-co+f;
+        
+    }
+};
