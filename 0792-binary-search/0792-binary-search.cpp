594ddf8c5eee1c03a355e7ba0900f4bec769bc8d
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-06-20 12:44:19 +0530

    Time: 28 ms (49.06%), Space: 29.9 MB (66.44%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1;
-            while (l <= h) {
-
-
-        int m=l+(h-l)/2;
-        if (nums[m]==target) return m;
-        if (target<nums[m]) h=m-1;
-        else { l=m+1; }
-
-
-            }
-            return -1;
-    }
- 
         
-    
+        int l=0,h=nums.size()-1,m=0;
+        while(l<=h){
+            m=l+(h-l)/2;
+            if(nums[m]==target) return m;
+            else if(nums[m]<target) l=m+1;
+            else h=m-1;
+
+        }
+        return -1;
+    }
 };
