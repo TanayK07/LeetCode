cb6356a176d7fd3888bc3e34b98690bc620c6f0f
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-09-28 00:55:56 +0530

    Time: 127 ms (5.08%), Space: 9.2 MB (98.73%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        int n = s.length();
+        for (int i = n-1; i >= 0; i--) {
+            if (s[i] == '-') {
+                s.erase(i, 1);
+            }
+
+        }
+        cout <<"erased"<<s<<endl;
+                int n1 = s.length();
+
+        for (int i = n1 - k; i > 0; i-=k) {
+            // if (i % k == 0)
+            cout <<i<<endl;
+                auto it = s.insert(s.begin() + i, '-');
+        }
+        transform(s.begin(), s.end(), s.begin(), ::toupper); 
+
+        return s;
+    }
+};
