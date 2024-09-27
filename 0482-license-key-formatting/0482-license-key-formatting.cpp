bbf5650662b55d0dc5db45ac7a78dab79a64cf57
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-09-28 02:03:07 +0530

    Time: 8 ms (53.69%), Space: 11.5 MB (13.51%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.cpp
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -2,22 +2,31 @@ class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
         int n = s.length();
-        for (int i = n-1; i >= 0; i--) {
+        string s2="", s4="";
+        int c=0;
+        for (int i = 0; i < n; i++) {
             if (s[i] == '-') {
-                s.erase(i, 1);
+                continue;
             }
-            if (s[i]>='a' && s[i]<='z') s[i]=s[i]-32;
-
+            if (s[i] >= 'a' && s[i] <= 'z') s2 += s[i] - 32;
+            else s2 += s[i];
         }
-        cout <<"erased"<<s<<endl;
-                int n1 = s.length();
 
-        for (int i = n1 - k; i > 0; i-=k) {
-            cout <<i<<endl;
-                auto it = s.insert(s.begin() + i, '-');
+        int n1 = s2.length();
+        int rem = n1 % k;
+        
+        if (rem > 0) {
+            s4 += s2.substr(0, rem);
         }
-        // transform(s.begin(), s.end(), s.begin(), ::toupper); 
+        // for (int i=n1;i
 
-        return s;
+        for (int i = rem; i < n1; i += k) {
+            if (!s4.empty()) {
+                s4 += "-";
+            }
+            s4 += s2.substr(i, k);
+        }
+
+        return s4;
     }
 };
