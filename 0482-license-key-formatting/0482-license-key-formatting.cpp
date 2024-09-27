be12d8b2d93d8cc5c0bc38abbc3d224212fa9b83
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-09-28 02:08:35 +0530

    Time: 102 ms (5.03%), Space: 9.9 MB (45.47%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.cpp
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
         int n = s.length();
+        vector<int> v1;
         for (int i = n-1; i >= 0; i--) {
             if (s[i] == '-') {
                 s.erase(i, 1);
@@ -11,11 +12,16 @@ public:
         }
         cout <<"erased"<<s<<endl;
                 int n1 = s.length();
+        // for 
 
         for (int i = n1 - k; i > 0; i-=k) {
-            cout <<i<<endl;
-                auto it = s.insert(s.begin() + i, '-');
+                // auto it = s.insert(s.begin() + i, '-');
+                v1.push_back(i);
         }
+        for(int i=0;i<v1.size();i++){
+            auto it=s.insert(s.begin()+v1[i],'-');
+        }
+
         // transform(s.begin(), s.end(), s.begin(), ::toupper); 
 
         return s;
