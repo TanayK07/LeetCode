ea20f18c3165e0b130792edb5ffbd9eaf05b020b
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-09-29 17:24:06 +0530

    Time: 90 ms (76.07%), Space: 54.3 MB (77.92%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -7,14 +7,9 @@ public:
 
 
     unordered_map<string,int> m1;
-    // multimap<int, string> MM;
     set<pair<int,string>> s1;
     
     void inc(string key) {
-        // if(key!="") m1[key]++;
-        // else m1[key]=1;
-        // MM.insert({m1[key],key});  
-        // cout<<"incr"<<m1[key]<<" "<<key<<endl;
         s1.erase({m1[key],key});
         m1[key]++;
         s1.insert({m1[key],key});
@@ -22,25 +17,14 @@ public:
     }
     
     void dec(string key) {
-        // if(m1[key]==0) m1.erase(key);
-        // else m1[key]--;
-        
-        // MM.insert({m1[key],key}); // syntax      
-        // cout<<"decr"<<m1[key]<<" "<<key<<endl;
         s1.erase({m1[key],key});
         m1[key]--;
-        if(m1[key]>0) s1.insert({m1[key],key});
-
-
-   
+        if(m1[key]>0) s1.insert({m1[key],key});  
 
     }
     
     
     string getMaxKey() {
-
-
-        // return prev(MM.end())->second;
         if(s1.size()==0) return "";
         return prev(s1.end())->second;
 
@@ -48,10 +32,7 @@ public:
     }
     
     string getMinKey() {
-        // cout<<"whole MAP"<<endl;
-        // for(auto it: MM) cout<<"MAp"<< it.first<<" "<<it.second<<endl;
         if(s1.size()==0) return "";
-
         return s1.begin()->second;
 
         
