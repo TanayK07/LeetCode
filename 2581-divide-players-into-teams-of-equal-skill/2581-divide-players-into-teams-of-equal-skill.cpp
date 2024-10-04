5949bbc208ff013273a25c446a7e8ed141b8bfdf
Tanay Kedia <95276223+TanayK07@users.noreply.github.com>
2024-10-04 13:54:37 +0530

    Time: 42 ms (99.6%), Space: 56.9 MB (19.2%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -3,35 +3,55 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         ll pres=0;
-        int n=skill.size();
-        sort(skill.begin(),skill.end());
-        ll fsum=skill[0]+skill[n-1];
-        ll twosum=0,twomul=0;
+        ll n=skill.size();
+        // sort(skill.begin(),skill.end());
+        // ll fsum=skill[0]+skill[n-1];
+        ll twosum=0,twomul=0,sum=0;
+        for (int i : skill) sum+=i;
+         if (sum % (n / 2) != 0) return -1;
+        ll tsum = sum / (n/2); 
+        unordered_map<ll,int> m1;
+        for (int i : skill) m1[i]++;
+    //     for (const auto& pair : m1) {
+    //     cout << "Element: " << pair.first << ", Count: " << pair.second << endl;
+    // }
+        for (auto it = m1.begin(); it != m1.end(); ++it) 
+              if (!m1.count(tsum - it->first) || m1[tsum - it->first] != it->second) 
+                return -1;
+            else 
+                pres += it->second * it->first * (tsum - it->first);
+            
+
+            return pres/2;
+
+    
+        // for (int i: skill) sum+=i;
+        // double l1=sum/double(n);
+        // if(l1!=)
+        // if(floor(l1) != ceil(l1)) return -1;
+        // cout<<l1<<endl;
         // for(int i=0;i<skill.size();i++)
         // int 
-        while(skill.size()!=0)
-        {
-            cout<<skill[0]<<" "<<skill[skill.size()-1]<<endl;
-            twosum=skill[0]+skill[skill.size()-1];
-            twomul=skill[0]*skill[skill.size()-1];
-            if(fsum!= twosum) return -1;
-            else {
-                cout<<"goes into else"<<endl;
-                    pres+=twomul;
+        // while(skill.size()!=0)
+        // {
+        //     cout<<skill[0]<<" "<<skill[skill.size()-1]<<endl;
+        //     twosum=skill[0]+skill[skill.size()-1];
+        //     twomul=skill[0]*skill[skill.size()-1];
+        //     // if(fsum!= twosum) return -1;
+        //     // else {
+        //     //     cout<<"goes into else"<<endl;
+        //     //         pres+=twomul;
 
-                skill.erase(skill.begin());
-                // skill.erase(skill.end()-1);
-                skill.pop_back();
-                cout<<skill.size()<<" : size"<<endl;
+        //     //     skill.erase(skill.begin());
+        //     //     // skill.erase(skill.end()-1);
+        //     //     skill.pop_back();
+        //     //     cout<<skill.size()<<" : size"<<endl;
 
-            }
-            // cout<<
+        //     // }
+        //     // cout<<
             
-        }
-        return pres;
-
-        
-
+        // }
+        return -1;
 
         
     }
